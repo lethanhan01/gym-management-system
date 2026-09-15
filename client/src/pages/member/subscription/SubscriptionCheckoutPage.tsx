28bd9e4916f6fd48289b0a2a925c2bcd1a5d37fc
lethanhan01 <An.LT235631@sis.hust.edu.vn>
2026-09-15 09:57:44 +0700

    Sửa lỗi thanh toán

--- client/src/pages/member/subscription/SubscriptionCheckoutPage.tsx
+++ client/src/pages/member/subscription/SubscriptionCheckoutPage.tsx
@@ -345,8 +345,8 @@ export default function SubscriptionCheckoutPage({ mode }: { mode: 'buy' | 'rene
             className="mt-auto"
           >
             {mode === 'renew'
-              ? t('subscription.checkout.buttonConfirmRenew', { price: formatVnd(state.price) })
-              : t('subscription.checkout.buttonConfirmPay', { price: formatVnd(state.price) })}
+              ? t('subscription.checkout.buttonConfirmRenew')
+              : t('subscription.checkout.buttonConfirmPay')}
           </Button>
         </Card>
 
